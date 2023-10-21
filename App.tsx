a9c944d9bfb7fe8373567c2fdcf23143894d8f1c
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-10-21 16:17:36 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  Text
+} from "react-native"
+
+function App(){
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>Hello Avinash !!!</Text>
+        <Text>Hello Avinash Pandey Ji!!!</Text>
+        <Text>Hello Avinash Pandey Ji!!!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
